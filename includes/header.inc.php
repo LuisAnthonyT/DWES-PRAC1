9bdceec7dcd9d4591d6722a1dd16806e27a05df0
luis anthony <luitoabol@alu.edu.gva.es>
2023-12-14 19:57:33 +0100

    RICK Y MORTY

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,66 +1,18 @@
 <?php
 /**
- *
- * @author Luis Anthony Toapanta Bolaños
+ *	@author Luis Anthony Toapanta Bolaños
+ *  @version 1.0
  */
 
-if (isset($_GET['logout']) && $_GET['logout'] == 1) {
-    // Destruir la sesión
-    session_destroy();
-
-    // Establecer el rol como 'invitado'
-    $_SESSION['rol'] = 'invitado';
-
-    // Redirigir al usuario a la página de inicio
-    header("Location: /index.php");
-    exit();
-}
- //IDIOMAS
- require_once(__DIR__ . '/lang/es.inc.php');
- $languages = ['es', 'ca', 'en'];
-
- if (isset($_GET['language'])) {
-   if (in_array($_GET['language'], $languages)) {
-       setcookie('language', $_GET['language'], time() + 3600);
-       header('Location: '.$_SERVER['PHP_SELF'].'');
-       exit();
-   }
- }
-
-if ($_SESSION['rol'] == 'invitado') { 
-    echo '<header>';
-        echo '<h1><a href="/index.php">'.$message['header.title'].'</a></h1>';
-        echo '<a href="/index.php">'.$message['header.main'].'</a>';
-        echo '<div class "containerLanguage">';
-          echo '<a href="/?language=es"><img class="icono" src="/img/languages/español.png" alt="español"></img></a>';
-          echo '<a href="/?language=ca"><img class="icono" src="/img/languages/valencià.png" alt="valencià"></img></a>';
-          echo '<a href="/?language=en"><img class="icono" src="/img/languages/english.png" alt="english"></img></a>';
-        echo '</div>';
-    echo '</header>';
-} else if ($_SESSION['rol'] == 'customer') {
-    echo '<header>';
-        echo '<h1><a href="/index.php">MerchaShop</a></h1>';
-        echo '<a href="/index">Principal</a></br>';
-        echo '<span>'.$_SESSION['userName'].'</span></br>';
-        echo '<a href="/?logout=1">Logout</a>';
-        echo '<div class "containerLanguage">';
-          echo '<a href="/?language=español"><img class="icono" src="/img/languages/español.png" alt="español"></img></a>';
-          echo '<a href="/?language=valencià"><img class="icono" src="/img/languages/valencià.png" alt="valencià"></img></a>';
-          echo '<a href="/?language=english"><img class="icono" src="/img/languages/english.png" alt="english"></img></a>';
-        echo '</div>';
-    echo '</header>';
-} else {
-    echo '<header>';
-        echo '<h1><a href="/index.php">MerchaShop</a></h1>';
-        echo '<a href="/index.php">Principal</a></br>';
-        echo '<span>'.$_SESSION['userName'].'</span></br>';
-        echo '<a href="/?logout=1">Logout</a></br>';
-        echo '<a href="/users.php">User</a>';
-        echo '<div class "containerLanguage">';
-          echo '<a href="/?language=español"><img class="icono" src="/img/languages/español.png" alt="español"></img></a>';
-          echo '<a href="/?language=valencià"><img class="icono" src="/img/languages/valencià.png" alt="valencià"></img></a>';
-          echo '<a href="/?language=english"><img class="icono" src="/img/languages/english.png" alt="english"></img></a>';
-        echo '</div>';
-    echo '</header>';
-}
+	echo '<header>';
+		echo '<ul>';
+			echo '<li><a href="/index.php/?name=rick">Rick</a></li>';
+			echo '<li><a href="/index.php/?name=morty">Morty</a></li>';
+		echo '</ul>';
+		echo '<form action="/index.php" method="get">';
+			echo '<label></label>';
+			echo '<input type="text" name="nameSearch">';
+			echo '<input type="submit" value="Buscar">';
+		echo '</form>';
+	echo '</header>';
 ?>
--- index.php
+++ index.php
@@ -1,109 +1,69 @@
 <?php
 /**
- *	Script que implementa un carrito de la compra con variables de sesión
- * 
- *	@author Álex Torres
+ *	@author Luis Anthony Toapanta Bolaños
+ *  @version 1.0
  */
-session_start();
+	if (isset($_GET['name']) && !empty($_GET['name'])) {
+		if ($_GET['name'] == 'rick') {
+				$data = file_get_contents("https://rickandmortyapi.com/api/character/?name=rick&page=2");
+				$data = json_decode($data);
 
-if (!isset($_SESSION['rol'])) {
-	$_SESSION['rol'] = 'invitado';
-}
-
-if(isset($_GET['add']) || isset($_GET['subtract']) || isset($_GET['remove'])) {
-	if(isset($_GET['add']) && $_GET['add']!='') {
-		if(!isset($_SESSION['basket'][$_GET['add']]))
-			$_SESSION['basket'][$_GET['add']] = 1;
-		else
-			$_SESSION['basket'][$_GET['add']] += 1;
-	}
-	if(isset($_GET['subtract']) && $_GET['subtract']!='' && isset($_SESSION['basket'][$_GET['subtract']])) {
-		$_SESSION['basket'][$_GET['subtract']] -= 1;
-		if($_SESSION['basket'][$_GET['subtract']]<=0)
-			unset($_SESSION['basket'][$_GET['subtract']]);
-	}
-	if(isset($_GET['remove']) && $_GET['remove']!='' && isset($_SESSION['basket'][$_GET['remove']])) {
-		unset($_SESSION['basket'][$_GET['remove']]);
+		} else if ($_GET['name'] == 'morty') {
+			$data = file_get_contents("https://rickandmortyapi.com/api/character/?name=morty&page=2");
+			$data = json_decode($data);
+		} 
 	}
 
-	//header('location: /');
-}
-//IDIOMAS
-if (isset($_COOKIE['language'])) {
-	require_once(__DIR__ . '/includes/lang/'.$_COOKIE['language'].'.inc.php');
-}
+	if (isset($_GET['nameSearch']) && !empty($_GET['nameSearch'])) {
+		$searchTerm = urlencode($_GET['nameSearch']);
 
-?>
-<!doctype html>
-<html lang="es">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title><?$message['head.title']?></title>
-		<link rel="stylesheet" href="/css/style.css">
-	</head>
-	<body>
-		<?php
-			require_once('includes/header.inc.php');
+		try {
+			$dataSearch = file_get_contents("https://rickandmortyapi.com/api/character/?name=$searchTerm&page=2");
+			$dataSearch = json_decode($data);
 
-			if ($_SESSION['rol'] == 'invitado') {
-		?>
-		<!-- USUARIO NO LOGUEADO -->
-		<form action="signup.php" method="post">
-    <?= $message['index.form.user']?><input type="text" name="user" value=""></br>
-		<?= $message['index.form.mail']?><input type="text" name="email" value=""></br>
-    <?= $message['index.form.password']?><input type="password" name="password" value=""></br>
-		<?= $message['index.form.password2']?><input type="password" name="password2" value=""></br>
-
-			<input type="submit" value="<?= $message['index.form.submit']?>">
-		</form>
-		<a href="/login.php"><?= $message['index.login']?></a></br>
-		<a href="/sales.php"><img  class="sales" src="/img/sales.png" alt="sales"></a>
-		<?php 
-			} else if ($_SESSION['rol'] == 'customer' || $_SESSION['rol'] == 'admin' ) {
-		?>
-		<!-- USUSARIO LOGUEADO -->
-		<div id="carrito">
-			<?php
-			if(!isset($_SESSION['basket']))
-				$products = 0;
-			else
-				$products = count($_SESSION['basket']);
-			echo $products;
-			echo ' producto';
-			if($products>1)
-				echo 's';
-			?>
-			en el carrito.
-
-			<a href="/basket.php" class="boton">Ver carrito</a>	
-		</div>
-		
-		<section class="productos">
-			<?php
-			require_once('includes/dbconnection.inc.php');
-			$connection = getDBConnection();
-			$products = $connection->query('SELECT * FROM products;', PDO::FETCH_OBJ);
-			
-			foreach($products as $product) {
-				echo '<article class="producto">';
-					echo '<h2>'. $product->name .'</h2>';
-					echo '<span>('. $product->category .')</span>';
-					echo '<img src="/img/products/'. $product->image .'" alt="'. $product->name .'" class="imgProducto"><br>';
-					echo '<span>'. $product->price .' €</span><br>';
-					echo '<span class="botonesCarrito">';
-						echo '<a href="/add/'.$product->id .'" class="productos"><img src="/img/mas.png" alt="añadir 1"></a>';
-						echo '<a href="/subtract/'.$product->id .'" class="productos"><img src="/img/menos.png" alt="quitar 1"></a>';
-						echo '<a href="/remove/'.$product->id .'" class="productos"><img src="/img/papelera.png" alt="quitar todos"></a>';
-					echo '</span>';
-					echo '<span>Stock: '. $product->stock .'</span>';
-				echo '</article>';
+			if ($dataSearch === null) {
+				throw new Exception("Error 404 :(");
 			}
 
-			unset($products);
-			unset($connection);
+		} catch (Exception $e) {
+			echo "Error" . $e->getMessage();
 		}
-			?>			
-		</section>		
-	</body>
+	}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/style.css">
+        <title>Rick y Morty</title>
+    </head>
+    <body>
+        <?php 
+        include_once(__DIR__ . '/includes/header.inc.php');
+
+				if (isset($data) && !empty($data)) {
+					echo '<div class="container">';
+							foreach ($data->results as $index => $character) {
+								echo '<article>';
+									echo '<img src="'.$character->image.'"></img>';
+									echo '<span>' . $character->name .'</span>';
+								echo '</article>';
+							}
+					echo '</div>';	
+				} 
+
+				if (isset($dataSearch) && !empty($dataSearch)) {
+					echo '<div class="container">';
+							foreach ($dataSearch->results as $index => $character) {
+								echo '<article>';
+									echo '<img src="'.$character->image.'"></img>';
+									echo '<span>' . $character->name .'</span>';
+								echo '</article>';
+							}
+					echo '</div>';	
+				} 
+    ?>
+</body>
 </html>
